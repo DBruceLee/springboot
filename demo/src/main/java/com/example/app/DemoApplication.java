bf8f9d5c952c2de6602ca1c442e2bd45ae73fabd
DBruceLee <1204504782@qq.com>
2018-04-27 09:27:14 +0800

    增加了一句话

--- demo/src/main/java/com/example/app/DemoApplication.java
+++ demo/src/main/java/com/example/app/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.example.demo.controller")
 public class DemoApplication {
     public static void main(String[] args) {
+        System.out.println("this is a good boy");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
