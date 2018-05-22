6874397cee3efed536159d9241a95dae5169e99a
DBruceLee <1204504782@qq.com>
2018-05-22 10:48:24 +0800

    更新静态配置类文件

--- demo/src/main/java/com/example/app/config/StaticSourceConfigs.java
+++ demo/src/main/java/com/example/app/config/StaticSourceConfigs.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class StaticSourceConfig extends WebMvcConfigurerAdapter {
+public class StaticSourceConfigs extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/*").addResourceLocations("classpath:/static/");
